55f18f2024176c41552e5afe4c894f623d9ea874
Thomas Ongeri <ongerit@gmail.com>
2017-09-26 14:17:11 -0400

    added slug, added display property

--- data/data.js
+++ data/data.js
@@ -6,61 +6,84 @@ var data = {
         {
             "active": true,
             "categoryLabel": "myTopcon",
+            "slug": "my-topcon",
+            "display": true,
             "menu": [
                 {
                     "label": "Topnet Live",
+                    "slug": "topnet-live",
                     "link": "https://example.com",
-                    "active": false
+                    "active": false,
+                    "display": true
                 },
                 {
                     "label": "MAGNET Enterprise",
+                    "slug": "magnet-enterprise",
                     "link": "https://example.com",
-                    "active": true
+                    "active": true,
+                    "display": false,
                 },
                 {
                     "label": "Site link 3D",
+                    "slug": "site-link-3d",
                     "link": "https://example.com",
-                    "active": false
+                    "active": false,
+                    "display": true
                 },
                 {
                     "label": "Site Planner",
+                    "slug": "site-planner",
                     "link": "https://example.com",
-                    "active": false
+                    "active": false,
+                    "display": true
                 },
                 {
                     "label": "Collage",
+                    "slug": "collage",
                     "link": "https://example.com",
-                    "active": false
+                    "active": false,
+                    "display": true
                 }
             ]
         }, {
             "active": false,
+            "slug": "products-and-services",
             "categoryLabel": "PRODUCTS AND SERVICES",
             "menu": [
                 {
                     "label": "Products and Subscriptions",
                     "link": "https://example.com",
-                    "active": false
+                    "slug": "products-and-subscriptions",
+                    "active": false,
+                    "display": false
                 },
                 {
                     "label": "GPShield",
+                    "slug": "gpshield",
                     "link": "https://example.com",
-                    "active": false
+                    "active": false,
+                    "display": true
                 },
                 {
                     "label": "TSshield",
+                    "slug": "tsshield",
                     "link": "https://example.com",
-                    "active": false
+                    "active": false,
+                    "display": true
                 },
                 {
                     "label": "Firmware Updater Utility",
+                    "slug": "firmware-updater-utility",
                     "link": "https://example.com",
-                    "active": false
+                    "active": false,
+                    "display": true
                 },
                 {
                     "label": "Support Cases",
+                    "slug": "support-cases",
                     "link": "https://example.com",
-                    "active": false
+                    "active": false,
+                    "display": true
                 }
             ]
         }
